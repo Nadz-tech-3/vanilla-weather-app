79bf91cd708fc303a80ef77cb35718bdbcba56b3
Nadz-tech-3 <135663084+Nadz-tech-3@users.noreply.github.com>
2023-08-20 19:36:06 +0100

    Added forecast days layout

--- src/app.js
+++ src/app.js
@@ -22,6 +22,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML + `   
+     <div class="col-2">
+     <div class="weather-forecast-date">${day}</div>
+     <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"  width="42" />
+     <div class="weather-forecast-temperatures">
+      <span class = "weather-forecast-temperature-max"> 18° </span>
+       <span class = "weather-forecast-temperature-min"> 12° </span>
+      </div>
+    </div>
+  `;
+});
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  }
+
+  
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -30,6 +52,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+  
+
 
 celsiusTemperature = response.data.temperature.current
 
@@ -79,6 +103,8 @@ function diplaycelsiusTemperature(event)
 
 let celsiusTemperature = null;
 
+  
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -87,3 +113,6 @@ fahrenheitLink.addEventListener("click", diplayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", diplaycelsiusTemperature);
+
+displayForecast();
+
