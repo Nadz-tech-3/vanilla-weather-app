fa6bfd36fef0d7baf006cd79d3bcf578cc8dd26b
Nadz-tech-3 <135663084+Nadz-tech-3@users.noreply.github.com>
2023-08-21 15:17:32 +0100

    forecast data using API added

--- src/app.js
+++ src/app.js
@@ -22,29 +22,51 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  return days[day];
+
+
+}
+
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function(day) {
-    forecastHTML = forecastHTML + `   
+  forecast.forEach(function(forecastDay, index) {
+     if (index < 6) {
+    forecastHTML = forecastHTML + 
+    `   
      <div class="col-2">
-     <div class="weather-forecast-date">${day}</div>
-     <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"  width="42" />
+     <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+     <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"  width="42" />
      <div class="weather-forecast-temperatures">
-      <span class = "weather-forecast-temperature-max"> 18° </span>
-       <span class = "weather-forecast-temperature-min"> 12° </span>
+      <span class = "weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+       <span class = "weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.minimum)}° </span>
       </div>
-    </div>
-  `;
+    </div>`;
+  } 
 });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  }
+}
 
   
-function displayTemperature(response) {
+function getForecast(coordinates){
+
+  let apiKey = "bfa005e48f0f8e59ta753oe5e3baa0b4";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=bfa005e48f0f8e59ta753oe5e3baa0b4&units=metric`;
+  axios.get(apiURL).then(displayForecast);
+}
+
+function displayTemperature(response)
+
+{
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -68,7 +90,16 @@ celsiusTemperature = response.data.temperature.current
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
+
+  getForecast(response.data.coordinates);
+
+  console.log(response.data);
+
+
+
 }
+
+
 
 function search(city) {
   let apiKey = "bfa005e48f0f8e59ta753oe5e3baa0b4";
@@ -114,5 +145,6 @@ fahrenheitLink.addEventListener("click", diplayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", diplaycelsiusTemperature);
 
-displayForecast();
+
+
 
